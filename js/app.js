a375cc470ae8f4e6e5f8de2fae48a2ebb6b8c101
Behruz <madaminovbehruz746@gmail.com>
2024-04-27 00:05:58 +0500

    added: runing first timer

--- js/app.js
+++ js/app.js
@@ -1,24 +1,71 @@
 function innerDOM() {
+  // DOM
   const settingsBtnsEl = document.querySelectorAll(".settings"),
     overSettingsEl = document.querySelector(".settings__over"),
     settingCloseBtnEl = document.querySelector(".setting__close__btn"),
     inputModeEl = document.querySelector(".mode"),
     inputAutoEl = document.querySelector(".auto__play"),
     inputMusicEl = document.querySelector(".music"),
-    inputNotifiEl = document.querySelector(".notifications");
+    inputNotifiEl = document.querySelector(".notifications"),
+    focusLengthEl = document.querySelector("#focus-length"),
+    timerPlayEl = document.querySelector("#timer-play"),
+    timerClockEl = document.querySelector("#timer-clock");
 
-  let toggleObj = {
-    modeToggle: false,
-    autoPlayToggle: false,
-    musicToggle: false,
-    notificationsToggle: false,
+  let settingsObj = {
+    toggles: {
+      modeToggle: false,
+      autoPlayToggle: false,
+      musicToggle: false,
+      notificationsToggle: false,
+      play: false,
+    },
+    inputVals: {
+      focusVal: focusLengthEl.value,
+    },
   };
+
+  function timerFun(num) {
+    let count = num * 60;
+    const timer = setInterval(function () {
+      let minutes = Math.floor(count / 60);
+      let seconds = count % 60;
+
+      timerClockEl.innerHTML = `${minutes.toString().padStart(2, "0")} ${seconds
+        .toString()
+        .padStart(2, "0")}`;
+
+      if (count === 0) {
+        clearInterval(timer);
+      } else {
+        count--;
+      }
+    }, 1000);
+  }
+
+  function playToggle() {
+    settingsObj.toggles["play"] = !settingsObj.toggles["play"];
+    timerClockEl.style.fontWeight = "800";
+
+    if (settingsObj.toggles["play"]) {
+      timerFun(settingsObj.inputVals["focusVal"]);
+      timerPlayEl.querySelector("img").src = "./assets/img/svg/pause.svg";
+    } else {
+      timerPlayEl.querySelector("img").src = "./assets/img/svg/play.svg";
+    }
+
+    console.log(settingsObj.toggles["play"]);
+  }
+
+  timerPlayEl.addEventListener("click", playToggle);
+  focusLengthEl.addEventListener("input", (e) => {
+    settingsObj.inputVals["focusVal"] = +e.target.value;
+  });
 
   // Setting inputs
 
   function inputToggler(obj, el) {
-    toggleObj[obj] = toggleObj[obj] ? false : true;
-    if (toggleObj[obj]) {
+    settingsObj.toggles[obj] = !settingsObj.toggles[obj];
+    if (settingsObj.toggles[obj]) {
       el.classList.add("input__toggle--active");
     } else {
       el.classList.remove("input__toggle--active");
@@ -46,6 +93,7 @@ function innerDOM() {
     ".timer"
   ).style.height = `calc(${document.documentElement.clientHeight}px - 120px)`;
 
+  // Events
   settingsBtnsEl.forEach((item) =>
     item.addEventListener("click", showOverSettings)
   );
